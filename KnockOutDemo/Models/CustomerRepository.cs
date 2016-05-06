40c5514c16a2c6ceeb5b2de6817414001baabfe9
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2016-05-06 11:10:15 +0300

    сущность/ контекст/ репозитарий/ строка подклюючения

---
+++ KnockOutDemo/Models/CustomerRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KnockOutDemo.Models
+{
+    public class CustomerRepository
+    {
+        private static CustomerContext db = new CustomerContext();
+ 
+        public static IEnumerable<Customer> GetCustomers()
+        {
+            var query = from cust in db.Customers
+                        select cust;
+
+            return query.ToList();  
+        }    
+
+
+        public static void InsertCustomer(Customer cust)
+        {       
+            db.Customers.Add(cust);
+            db.SaveChanges();
+        }
+
+    }
+}
