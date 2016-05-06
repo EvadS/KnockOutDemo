930b66bc26ecdd01478e32a9abca9f1c02862fe9
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2016-05-06 13:09:52 +0300

    Добавил страницу макета по умолчанию/

--- KnockOutDemo/App_Start/BundleConfig.cs
+++ KnockOutDemo/App_Start/BundleConfig.cs
@@ -11,9 +11,9 @@ namespace KnockOutDemo.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            // в режиме отладки пакетирование отключено по умолчанию
+            //TODO в режиме отладки пакетирование отключено по умолчанию
             // нужного его включить 
-            //BundleTable.EnableOptimizations = true;
+           // BundleTable.EnableOptimizations = true;
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
              "~/Scripts/jquery-{version}.js"));
